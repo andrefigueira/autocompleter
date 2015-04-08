4808f8dc5b2f46bb4577ae6effcd5ae83a714e49
Andre Figueira <andre.figueira@me.com>
2015-04-08 23:08:43 +0100

    - Initial plugin setup - Some mods to the bower config

---
+++ src/autocompleter.js
@@ -0,0 +1,15 @@
+/**
+ * A simple autocomplete plugin in JavaScript
+ *
+ * @author Andre Figueira <andre.figueira@me.com>
+ * @version 0.0.1
+ */
+$.fn.autocompleter = function() {
+
+    var options = {
+    };
+
+    var options = $.extend(options, options);
+
+
+};
