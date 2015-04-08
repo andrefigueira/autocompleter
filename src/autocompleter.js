26a5117f58bfa7107a88d04f1b90c04da9a4e593
Andre Figueira <andre.figueira@me.com>
2015-04-09 00:54:23 +0100

    - Made initial autocomplete work

--- src/autocompleter.js
+++ src/autocompleter.js
@@ -4,12 +4,81 @@
  * @author Andre Figueira <andre.figueira@me.com>
  * @version 0.0.1
  */
-$.fn.autocompleter = function() {
+$.fn.autocompleter = function(settings) {
 
-    var options = {
-    };
+    var options = $.extend({
+        resultsSelector: 'autocompleter-results',
+        'source': function(query, results) {}
+    }, settings || {});
 
-    var options = $.extend(options, options);
+    var input = $(this);
 
+    /**
+     * Lets attach a keyup event on the input
+     */
+    $(document).on('keyup', input, function(){
+        var query = input.val();
+
+        // Check for results in the callback
+        options.source.call(this, query, function(sourceData){
+            if (typeof sourceData !== 'object') {
+                return false;
+            }
+
+            var autocompleterHtmlSelectorKey = options.resultsSelector;
+            var autocompleterHtmlSelector = '.' + autocompleterHtmlSelectorKey;
+
+            if (query == '') {
+                if ($(autocompleterHtmlSelector).size() > 0) {
+                    $(autocompleterHtmlSelector).hide();
+                }
+            } else {
+                if (sourceData.length > 0) {
+                    if ($(autocompleterHtmlSelector).size() > 0) {
+                        var itemResults = generateResultsItemsHtml(sourceData);
+
+                        $(autocompleterHtmlSelector).html(itemResults);
+                        $(autocompleterHtmlSelector).show();
+                    } else {
+                        var mainHtml = generateResultsHtml(sourceData);
+
+                        input.after(mainHtml);
+                    }
+                } else {
+                    if ($(autocompleterHtmlSelector).size() > 0) {
+                        $(autocompleterHtmlSelector).hide();
+                    }
+                }
+            }
+        });
+    });
+
+    /**
+     * Generates the full autocomplete dropdown, used on initial search
+     *
+     * @param sourceData
+     * @returns {string}
+     */
+    function generateResultsHtml(sourceData) {
+        var itemResults = generateResultsItemsHtml(sourceData);
+
+        return '<ul class="autocompleter ' + options.resultsSelector + '">' + itemResults + '</ul>';
+    }
+
+    /**
+     * Generates items html, used for initial call and subsequent calls
+     *
+     * @param data
+     * @returns {string}
+     */
+    function generateResultsItemsHtml(data) {
+        var html = '';
+
+        $.each(data, function(index, item){
+            html += '<li>' + item.name + '</li>';
+        });
+
+        return html;
+    }
 
 };
