886cc7a44c3c98186f5cae905df6d486567e2b4f
Andre Figueira <andre.figueira@me.com>
2015-04-09 09:55:33 +0100

    - Made the formatter work - Added some keyup events for setting items as active on the resultset

--- src/js/autocompleter.js
+++ src/js/autocompleter.js
@@ -7,26 +7,97 @@
 $.fn.autocompleter = function(settings) {
 
     var options = $.extend({
+        format: null,
+        activeClass: 'active',
+        formatPlaceholderRegex: /\{\{ (.*?) \}\}/g,
         resultsSelector: 'autocompleter-results',
+        nameKey: 'name',
         'source': function(query, results) {}
     }, settings || {});
 
     var input = $(this);
+    var autocompleterHtmlSelectorKey = options.resultsSelector;
+    var autocompleterHtmlSelector = '.' + autocompleterHtmlSelectorKey;
+
+    var UP_KEY = 38;
+    var DOWN_KEY = 40;
+    var ENTER_KEY = 13;
+    var ESCAPE_KEY = 27;
 
     /**
      * Lets attach a keyup event on the input
      */
-    $(document).on('keyup', input, function(){
+    $(document).on('keyup', input, function(e){
         var query = input.val();
+
+        // Lets bind some specific key events on escape hide results
+        if (e.keyCode === ESCAPE_KEY) {
+            hideResults();
+
+            return false;
+        }
+
+        // Lets bind the down key event to highlight the currently selected result item
+        if (e.keyCode === DOWN_KEY) {
+            var currentActiveItemSelector = autocompleterHtmlSelector + ' li.' + options.activeClass;
+            if ($(currentActiveItemSelector).size() > 0) {
+                var nextItem = $(currentActiveItemSelector).next('li');
+
+                if (nextItem.size() > 0) {
+                    $(currentActiveItemSelector).removeClass(options.activeClass);
+                    nextItem.addClass(options.activeClass);
+                }
+            } else {
+                if ($(autocompleterHtmlSelector + ' li').size() > 0) {
+                    $(autocompleterHtmlSelector + ' li').first().addClass(options.activeClass);
+                }
+            }
+
+            return false;
+        }
+
+        // Lets bind the up key to highlight the currently selected result item
+        if (e.keyCode === UP_KEY) {
+            var currentActiveItemSelector = autocompleterHtmlSelector + ' li.' + options.activeClass;
+            if ($(currentActiveItemSelector).size() > 0) {
+                var nextItem = $(currentActiveItemSelector).prev('li');
+
+                if (nextItem.size() > 0) {
+                    $(currentActiveItemSelector).removeClass(options.activeClass);
+                    nextItem.addClass(options.activeClass);
+                } else {
+                    $(currentActiveItemSelector).removeClass(options.activeClass);
+                }
+            } else {
+                if ($(autocompleterHtmlSelector + ' li').size() > 0) {
+                    $(autocompleterHtmlSelector + ' li').first().addClass(options.activeClass);
+                }
+            }
+
+            return false;
+        }
+
+        // Lets bind the enter key to go to a href data action as the window location if there is an active result item
+        if (e.keyCode === ENTER_KEY) {
+            var currentActiveItemSelector = autocompleterHtmlSelector + ' li.' + options.activeClass;
+
+            if ($(currentActiveItemSelector).size() > 0) {
+
+                var activeItem = $(currentActiveItemSelector);
+
+                // @todo: Do some action on press enter
+
+                e.preventDefault();
+                e.stopPropagation();
+                return false;
+            }
+        }
 
         // Check for results in the callback
         options.source.call(this, query, function(sourceData){
             if (typeof sourceData !== 'object') {
                 return false;
             }
-
-            var autocompleterHtmlSelectorKey = options.resultsSelector;
-            var autocompleterHtmlSelector = '.' + autocompleterHtmlSelectorKey;
 
             if (query == '') {
                 if ($(autocompleterHtmlSelector).size() > 0) {
@@ -54,6 +125,26 @@ $.fn.autocompleter = function(settings) {
     });
 
     /**
+     * Lets setup a body click event to hide the results if we click away from them
+     */
+    $(document).on('click', $('body'), function(){
+        hideResults();
+    });
+
+    /**
+     * Lets not hide the results if we click them!
+     */
+    $(document).on('click', autocompleterHtmlSelector, function(e){
+        e.stopPropagation();
+    });
+
+    function hideResults() {
+        if ($(autocompleterHtmlSelector).size() > 0) {
+            $(autocompleterHtmlSelector).hide();
+        }
+    }
+
+    /**
      * Generates the full autocomplete dropdown, used on initial search
      *
      * @param sourceData
@@ -74,9 +165,18 @@ $.fn.autocompleter = function(settings) {
     function generateResultsItemsHtml(data) {
         var html = '';
 
-        // @todo: Make the item information here customisable
         $.each(data, function(index, item){
-            html += '<li>' + item.name + '</li>';
+            if (options.format == null) {
+                html += '<li>' + item.name + '</li>';
+            } else {
+                var htmlContent = '<li>' + options.format + '</li>';
+
+                html += htmlContent.replace(options.formatPlaceholderRegex, function(match, token){
+
+                    return item[token];
+
+                });;
+            }
         });
 
         return html;
